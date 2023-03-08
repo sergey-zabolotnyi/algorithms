c717a6167af187660ce2ce5817b07c8f2d7c22a0
SergeyZ <szabolotnyi777@gmail.com>
2023-03-08 22:06:56 +0100

    homework 07.03.2023

--- src/homeWorks/homework01_07_03_2023/findSingleElement.java
+++ src/homeWorks/homework01_07_03_2023/findSingleElement.java
@@ -2,16 +2,16 @@ package homeWorks.homework01_07_03_2023;
 
 public class findSingleElement {
     public static void main(String[] args) {
-        int[] newArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 9}; // пример массива (8 в одном экземпляре)
+        int[] newArr = {1, 2, 3, 4, 5, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // пример массива (6 в одном экземпляре)
 
         int uniqueNum = findSingleElement(newArr);
-        System.out.println("В массиве только данный элемент: " + uniqueNum + " в 1 эксемпляре"); // ищем 8
+        System.out.println("В массиве только данный элемент: " + uniqueNum + " в 1 эксемпляре"); // ищем 6
 
     }
     public static int findSingleElement(int[] arr) {
         int result = 0;
-        for (int num : arr) {
-            result ^= num;
+        for (int i = 0; i < arr.length; i++) {
+            result ^= arr[i];
         }
         return result;
     }
