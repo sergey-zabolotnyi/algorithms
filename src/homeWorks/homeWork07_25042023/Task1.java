384a0c14b9e23bd8c40eab14dd7eaf1872dfe5c2
SergeyZ <szabolotnyi777@gmail.com>
2023-04-26 18:27:46 +0200

    HomeWork7 25.04.2023

--- src/homeWorks/homeWork07_25042023/Task1.java
+++ src/homeWorks/homeWork07_25042023/Task1.java
@@ -13,6 +13,7 @@ public class Task1 {
         int[] array2 = {4,3,2,1};
         int[] array3 = {9};
         int[] array4 = new int[]{9,9,9,9};
+
         System.out.println(Arrays.toString(convertArr(array1))); // Output: [1,2,4]
         System.out.println(Arrays.toString(convertArr(array2))); // Output: [4,3,2,2]
         System.out.println(Arrays.toString(convertArr(array3))); // Output: [1,0]
@@ -20,22 +21,28 @@ public class Task1 {
         
     }
     public static int[] convertArr(int[] arr) {
-        int num = 0;
+        int number = 0;
         // 1. Переводим массив в число
         for (int i = 0; i < arr.length; i++) {
-            num = num * 10 + arr[i];
+            number = number * 10 + arr[i];
             //System.out.println(num);
         }
-        // 2. Увеличиваем на 1
-        num += 1;
+        // 2. Увеличиваем число на 1
+        number += 1;
         // 3.Узнаем количество элементов нового массива
-        int length = String.valueOf(Math.abs(num)).length();
+        int temp = number;
+        int length = 0;
+        while (temp != 0){
+            length++;
+            temp = temp / 10;
+        } // System.out.println(length);
+
         // 4. Создаем новый массив
         int[] arr1 = new int[length];
         // 5. Заполняем элементами
         for (int i = length - 1; i >= 0; i--) {
-            arr1[i] = num % 10;
-            num /= 10;
+            arr1[i] = number % 10;
+            number /= 10;
             //System.out.println(arr1[i]);
         }
         return arr1;
