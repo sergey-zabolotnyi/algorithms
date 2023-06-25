18ed7480ceca4bcd3a013415eae854ede4c8767c
SergeyZ <szabolotnyi777@gmail.com>
2023-06-25 14:02:39 +0200

    HomeWork15 20.06.2023

---
+++ src/homeWorks/hw15_20062023/Main.java
@@ -0,0 +1,15 @@
+package homeWorks.hw15_20062023;
+
+public class Main {
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(5);
+//        tree.insert(2);
+//        tree.insert(6);
+//        tree.insert(5);
+//        tree.insert(20);
+//        tree.insert(30);
+        tree.print();
+    }
+}
